338e4674bdf5fc6d9b6292afde5788d119cb883a
rednafi <redowan.nafi@gmail.com>
2023-04-19 14:38:26 -0400

    Keep both partial and preliminary fields, refs main

--- backtick/worker.py
+++ backtick/worker.py
@@ -6,29 +6,35 @@ import logging
 
 from rq import Worker
 
-from . import settings, utils
+from . import (
+    settings,
+    # We're importing the tasks module here to reduce any task setup cost after forking.
+    tasks,  # noqa
+    utils,
+)
 
 
 def main() -> None:
     """Run the worker."""
     parser = argparse.ArgumentParser()
+
+    # Accept a flag to run the scheduler as well.
     parser.add_argument(
         "--with-scheduler", action="store_true", help="Run the scheduler as well."
     )
     # Accept a list of queues to listen to.
     parser.add_argument(
-        "--queue-name", type=str, nargs="+", help="The name of the queue to listen to."
+        "--queue-names", type=str, nargs="+", help="The name of the queue to listen to."
     )
 
     args = parser.parse_args()
     with_scheduler = args.with_scheduler
+    queue_names = args.queue_names
 
-    # Provide the worker with the list of queues (str) to listen to.
     w = Worker(
-        queues=settings.BACKTICK_QUEUES.values(),
+        queues=queue_names if queue_names else settings.BACKTICK_QUEUES.values(),
         connection=utils.get_redis(),
     )
-
     logging.info("Starting worker, scheduler: %s", with_scheduler)
     w.work(with_scheduler=with_scheduler)
 
--- scripts/shutdown_workers.py
+++ scripts/shutdown_workers.py
@@ -1,1 +1,17 @@
 """Shutdown all workers."""
+from rq.command import send_shutdown_command
+from rq.worker import Worker
+
+from backtick import utils
+
+
+def main() -> None:
+    connection = utils.get_redis()
+
+    workers = Worker.all(connection=connection)
+    for worker in workers:
+        send_shutdown_command(connection, worker.name)  # Tells worker to shutdown
+
+
+if __name__ == "__main__":
+    main()
